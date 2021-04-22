88d86876b4723f4e214fcba6c02ee5eaf1a8df26
Cristian Prochnow <cristianprochnow@gmail.com>
2021-04-22 16:09:11 -0300

    feat(pages/PlantSelector.tsx): add logic to select an environment

--- src/pages/PlantSelector.tsx
+++ src/pages/PlantSelector.tsx
@@ -1,17 +1,58 @@
-import React from 'react'
+import React, {
+  useEffect,
+  useState
+} from 'react'
 import {
   Text,
   View,
   FlatList
 } from 'react-native'
 
-import headerAvatar from '../assets/cristian.png'
 import { EnvironmentButton } from '../components/EnvironmentButton'
 import { Header } from '../components/Header'
+import { api } from '../services/api'
+import headerAvatar from '../assets/cristian.png'
 import { styles } from '../styles/pages/plantSelector'
 
+interface EnvironmentData {
+  key: string
+  title: string
+}
+
 export const PlantSelector = () => {
-  function handleSelectEnvironment() {}
+  const [environments, setEnvironments] = useState<EnvironmentData[]>([])
+  const [selectedEnvironments, setSelectedEnvironments] = useState<string[]>([])
+
+  function handleToggleEnvironment(environmentKey: string) {
+    const isEnvironmentAtArray = selectedEnvironments.includes(environmentKey)
+
+    if (isEnvironmentAtArray) {
+      unselectEnvironment(environmentKey)
+    } else {
+      selectEnvironment(environmentKey)
+    }
+
+    function selectEnvironment(environmentKey: string) {
+      setSelectedEnvironments([...selectedEnvironments, environmentKey])
+    }
+
+    function unselectEnvironment(environmentKey: string) {
+      const filteredEnvironments = selectedEnvironments
+        .filter(environmentItem => environmentItem !== environmentKey)
+
+      setSelectedEnvironments(filteredEnvironments)
+    }
+  }
+
+  useEffect(() => {
+    fetchPlantEnvironmentData()
+
+    async function fetchPlantEnvironmentData() {
+      const {data} = await api.get('/plants_environments')
+
+      setEnvironments(data)
+    }
+  }, [])
 
   return (
     <View style={styles.container}>
@@ -38,12 +79,13 @@ export const PlantSelector = () => {
 
       <View>
         <FlatList
-          data={[1, 2, 3, 4, 5]}
-          renderItem={() => (
+          data={environments}
+          renderItem={({item}) => (
             <EnvironmentButton
-              label="Cozinha"
-              isSelected={false}
-              onPress={handleSelectEnvironment}
+              key={item.key}
+              label={item.title}
+              isSelected={selectedEnvironments.includes(item.key)}
+              onPress={() => handleToggleEnvironment(item.key)}
             />
           )}
           horizontal
